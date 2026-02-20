a888a6e7fc56f94b45796b2b576fc20681ddc232
zappian-sunil <sunil.patel@zappian.com>
2026-02-20 11:47:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [question, setQuestion] = useState("");
+  const [response, setResponse] = useState("");
+  const APIKEY="AIzaSyApsyfqiguuahOyDq_1ik3zmDJP68B2qgQ";
+
+  const handelQuestion = async () => {
+    const res = await fetch(
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-goog-api-key": "AIzaSyDi-wqUAlmfeFBXv_Y_DJV5DeeVoR-qT3s"
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [{ text: "Explain how AI works in a few words" }]
+            }
+          ]
+        })
+      }
+    );
+
+    const data = await res.json();
+    console.log(data)
+    setResponse(data.candidates[0].content.parts[0].text);
+  };
+
+
+
+
+
+
+
+  return (
+    <>
+ <div className="grid grid-cols-5 h-screen text-center">
+<div className="col-span-1 bg-zinc-800 ">
+asbc
+</div>
+
+<div className="col-span-4">
+<div className="container h-110 text-white">
+dsfdsfdsfdsf
+
+ <p>{response}</p>
+</div>
+<div className="bg-zinc-800 w-1/2 mx-auto flex items-center p-3 rounded-2xl shadow-lg border border-zinc-700">
+  
+  <input 
+    type="text"
+    value={question}
+    onChange={(event)=>setQuestion(event.target.value)}  
+    placeholder="Send a message..." 
+    className="flex-1 bg-transparent text-white placeholder-zinc-400 outline-none px-2"
+  />
+
+  <button onClick={handelQuestion} className="text-white bg-green-600 hover:bg-green-500 p-2 rounded-xl">
+    ➤
+  </button>
+</div>
+
+
+
+</div>
+
+
+ </div>
+
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+
+const CURL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
